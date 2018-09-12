920f273e38c6bda1220e04ceaa60ac124418f542
ymwchina <yumuwu@live.cn>
2018-09-12 18:28:00 +0800

    上传到本地版本控制库ProductController
    index test
    
    second hi..
    
    注释 product controller

--- src/main/java/com/oa/controller/ProductController.java
+++ src/main/java/com/oa/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.oa.controller;
-import com.oa.model.Product;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+//import com.oa.model.Product;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
 public class ProductController {
-    @RequestMapping("/addProduct")
-    public ModelAndView add(Product product) throws Exception{
-        ModelAndView mav=new ModelAndView("showProduct");
-        return mav;
-    }
+//    @RequestMapping("/addProduct")
+//    public ModelAndView add(Product product) throws Exception{
+//        ModelAndView mav=new ModelAndView("showProduct");
+//        return mav;
+//    }
 
 }
